b44af94d201bbbbedfedf93e49933b250f8e73da
Macair <aryamannatrajan18@gmail.com>
2026-06-26 04:43:32 +0530

    chore(release): prepare v0.2.0-alpha

--- src/osef/__init__.py
+++ src/osef/__init__.py
@@ -4,4 +4,9 @@ OSEF Core Package.
 This package exposes the programmatic SDK interface.
 """
 
-__version__ = "0.1.0"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("osef")
+except PackageNotFoundError:
+    __version__ = "unknown"
